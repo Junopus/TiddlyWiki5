4ee055496c33a23b94c72ac4f8883be6b1551f57
Junopus <tknjn2@gmail.com>
2017-12-27 12:45:15 +0900

    Added manual size specification.

--- plugins/resize-image/resize-image.js
+++ plugins/resize-image/resize-image.js
@@ -14,23 +14,17 @@ Bitmap editor operation to resize the image
 
 exports["resize-image"] = function(event) {
     // Get the new size
-    var maxSize = parseInt(event.paramObject.resizeTo,10);
+    var newWidth = parseInt(event.paramObject.width || this.canvasDomNode.width,10);
     // Update if necessary
-    if(maxSize < this.currCanvas.width || maxSize < this.currCanvas.height) {
+    if((newWidth > 0) && (newWidth < this.currCanvas.width)) {
         // Calc new size
-        var newWidth = maxSize, newHeight = maxSize;
-        if(this.currCanvas.width > this.currCanvas.height) {
-            newHeight = Math.floor(this.currCanvas.height * newWidth / this.currCanvas.width);
-        } else {
-            newWidth = Math.floor(this.currCanvas.width * newHeight / this.currCanvas.height);
-        }
-
+        var newHeight = Math.floor(this.currCanvas.height * newWidth / this.currCanvas.width);
         // Create and size a new canvas
         var newCanvas = this.document.createElement("canvas");
-        this.initCanvas(newCanvas,newWidth,newHeight);
+        this.initCanvas(newCanvas, newWidth, newHeight);
         // Copy the old image
         var ctx = newCanvas.getContext("2d");
-        ctx.drawImage(this.currCanvas,0,0,newWidth,newHeight);
+        ctx.drawImage(this.currCanvas, 0, 0, newWidth, newHeight);
         // Set the new canvas as the current one
         this.currCanvas = newCanvas;
         // Set the size of the onscreen canvas
